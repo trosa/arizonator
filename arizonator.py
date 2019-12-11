d96748ebf21049f8f25a2db70607b33f4e57fb89
Vitor França <vitufr@gmail.com>
2019-12-11 13:48:36 -0300

    remove prints

--- arizonator.py
+++ arizonator.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template
 from configparser import ConfigParser
 import requests
 from datetime import datetime
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
 
 def get_querystring(config):
     api_key = config['WeatherBit']['api_key']
@@ -39,14 +36,11 @@ def index():
     icons = {}
 
     for date in weatherdata:
-        pp.pprint(date)
-        print("\n")
         year, month, day = tuple([int(d) for d in date["valid_date"].split("-")])
         weekday = datetime(year, month, day).strftime("%A")
         if weekday in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
             rains[weekday] = date["precip"]
             icons[weekday] = "https://weatherbit.io/static/img/icons/" + date["weather"]["icon"] + ".png"
-            # print(weekday, rains[weekday])
 
     arizonaday = config['Defaults']['arizona_day']
     if datetime.today().weekday() > 1:
